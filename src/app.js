15373f5efea8e61574fb48e34d781b0e7e569326
Caiua Gomes de Franca <caiua.franca@ale.com.br>
2020-06-16 09:14:51 -0300

    Update Desafio

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,15 +10,44 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {url, title, techs} = request.body
+
+  const repository = {
+    id: uuid(),
+    url: url,
+    title: title,
+    techs: techs,
+    likes: 0
+  }
+
+  repositories.push(repository)
+
+  return response.json(repository)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params
+  const{url,title,techs,likes} = request.body
+
+  const repositoryIndex = repositories.findIndex(element => element.id == id)
+
+  if(repositoryIndex < 0 ){
+    response.status(400).json({error: 'Repository not found'})
+  }
+
+  const repository = {
+      id,
+      url,
+      title,
+      techs,
+      likes
+  }  
+  repositories[repositoryIndex] = repository
+  return response.json(repository)
 });
 
 app.delete("/repositories/:id", (request, response) => {
