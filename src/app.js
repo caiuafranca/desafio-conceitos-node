a560524d66cd78ea907f2c81284a14f872717b06
Caiua Gomes de Franca <caiua.franca@ale.com.br>
2020-06-16 10:10:44 -0300

    Ajuste Update Like

--- src/app.js
+++ src/app.js
@@ -31,14 +31,15 @@ app.post("/repositories", (request, response) => {
 
 app.put("/repositories/:id", (request, response) => {
   const {id} = request.params
-  const{url,title,techs,likes} = request.body
+  const{url,title,techs} = request.body
 
   const repositoryIndex = repositories.findIndex(element => element.id == id)
 
   if(repositoryIndex < 0 ){
     response.status(400).json({error: 'Repository not found'})
   }
-
+  const likes = repositories[repositoryIndex].likes
+  
   const repository = {
       id,
       url,
@@ -51,11 +52,39 @@ app.put("/repositories/:id", (request, response) => {
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params
+
+  const repositoryIndex = repositories.findIndex(element => element.id == id)
+
+  if(repositoryIndex < 0 ){
+    response.status(400).json({error: 'Repository not found'})
+  }
+
+  const repository = repositories[repositoryIndex]
+
+  repositories.splice(repository, 1);
+
+  return response.json({message:"Data deleted."})
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params
+
+  const repositoryIndex = repositories.findIndex(element => element.id == id)
+
+  if(repositoryIndex < 0 ){
+    response.status(400).json({error: 'Repository not found'})
+  }
+
+  const repository = repositories[repositoryIndex]
+
+  repository.likes = repository.likes + 1
+
+  repositories[repositoryIndex] = repository
+
+  return response.json(repository)
+
 });
 
 module.exports = app;
